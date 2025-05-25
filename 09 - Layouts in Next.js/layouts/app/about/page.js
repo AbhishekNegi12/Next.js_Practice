7dcafdaa8d52bfba8c6a6479ca5f2943b959baa2
AbhishekNegi12 <47.negi@gmail.com>
2025-05-25 18:53:04 +0530

    Layout files in next.js

---
+++ 09 - Layouts in Next.js/layouts/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio facere nemo repellat fuga blanditiis nisi doloremque, nihil non voluptate, eligendi illo quibusdam nam repellendus. Laboriosam, aperiam. Debitis aliquid provident assumenda vel possimus? Velit, ad odit!</div>
+  )
+}
+
+export default page
---
+++ 09 - Layouts in Next.js/layouts/app/admin/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Admin Login
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis eius veniam omnis impedit odio debitis ex. Numquam eos excepturi sunt, voluptatem consectetur nobis esse! Corrupti sapiente ipsum explicabo minus ipsam recusandae reprehenderit praesentium unde!            
+        </p>
+    </div>
+  )
+}
+
+export default page
